89954daf79672653f9f3c6e6e4fd327a9b7e7c92
glazeni <glazen@protonmail.com>
2016-12-19 00:25:01 +0000

    TabFragmentOne inside ThirdFragment is working using just Adapter

--- app/src/main/java/internetmeasurement/android/ThirdFragment.java
+++ app/src/main/java/internetmeasurement/android/ThirdFragment.java
@@ -1,51 +1,89 @@
 package internetmeasurement.android;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ThirdFragment extends Fragment implements TabLayout.OnTabSelectedListener {
-    TabLayout tabLayout;
     ViewPager viewPager;
-    PagerAdapterThirdFragment pagerAdapter;
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View thirdView = inflater.inflate(R.layout.fragment_third, container, false);
 
-        View thirdView = inflater.inflate(R.layout.fragment_third,container,false);
-        //TabLayout
-        tabLayout = (TabLayout) thirdView.findViewById(R.id.tabs);
+        //setHasOptionsMenu(true);
 
-        //Initializing View Pager
-        viewPager = (ViewPager) thirdView.findViewById(R.id.viewpager);
+        ViewPager viewPager = (ViewPager) thirdView.findViewById(R.id.viewpager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
 
-        //Creating PagerAdapter
-        pagerAdapter = new PagerAdapterThirdFragment(getFragmentManager(),2);
-
-        //Adding adapter to pager
-        viewPager.setAdapter(pagerAdapter);
-
-        //Wiring tabLayout with Viewpager
+        Log.v("Layout", "Tabs");
+        TabLayout tabLayout = (TabLayout) thirdView.findViewById(R.id.tabs);
+        tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#FFFFFF"));
+        tabLayout.setTabTextColors(Color.parseColor("#707070"), Color.parseColor("#FFFFFF"));
+        assert viewPager != null;
         tabLayout.setupWithViewPager(viewPager);
 
-        //Adding onTabSelectedListener to swipe views
-        tabLayout.setOnTabSelectedListener(this);
-
-        if(tabLayout != null) {
-            setupTabTitles();
-        }
         return thirdView;
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getChildFragmentManager());
+        adapter.addFragment(new TabFragmentOne(), "PHOTOS");
+        adapter.addFragment(new TabFragmentOne(), "HI-FIVES");
+        viewPager.setAdapter(adapter);
+    }
+
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
+    }
+
+
+
 
     @Override
     public void onTabSelected(TabLayout.Tab tab) {
@@ -60,9 +98,5 @@ public class ThirdFragment extends Fragment implements TabLayout.OnTabSelectedLi
     @Override
     public void onTabReselected(TabLayout.Tab tab) {
 
-    }
-    private void setupTabTitles(){
-        tabLayout.getTabAt(0).setText("Network Info");
-        tabLayout.getTabAt(1).setText("Settings");
     }
 }
